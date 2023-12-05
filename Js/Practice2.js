e8f5c1f6548817865aea7f0051f969e4771911b7
Sri-Mayur <mayursrivastava877@gmail.com>
2023-12-05 12:18:52 +0530

    5 dec 2023

---
+++ Js/Practice2.js
@@ -0,0 +1,15 @@
+let time = new Date();
+
+console.log(time);
+
+let date = new Date(2023, 0, 26);
+
+console.log(date);
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getMilliseconds());
+console.log(date.getSeconds());
+
---
+++ practice.js
@@ -0,0 +1,37 @@
+//Find the area of cylinder
+//Write a js program to display the reading status 
+
+/*var r= prompt("Enter the radius:","");
+var h= prompt("Enter the height:"," ");
+let a= 3.14*r*r*h;
+*/
+var z= document.getElementById('u');
+
+
+
+var library=[
+{
+    Name:"Author",
+    Book:"Cool",
+    Technique:"Super Cool",
+    Reading_Status:"True"
+},
+{
+    Name:"Author2",
+    Book:"SO COOL",
+    Technique:"Super Cool",
+    Reading_Status:"True"
+
+},
+{
+    Name:"Author3",
+    Book:"Cool Cool",
+    Technique:"Super Cool",
+    Reading_Status:"True"
+}];
+
+
+
+console.log(library[0].Name,library[0].Reading_Status);
+console.log(library[1].Name,library[1].Reading_Status);
+console.log(library[2].Name,library[2].Reading_Status);
